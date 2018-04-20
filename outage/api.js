2b736dc8d43e8ea018cf8faca2102d3d70833314
Stefano <stefanomeucci@hotmail.com>
2018-04-20 09:29:43 +0200

    Modifications after pull request review

--- outage/api.js
+++ outage/api.js
@@ -6,67 +6,35 @@ let fs = require('fs');
 var app = express();
 
 
+
 /**
- * returns the orders that have a type set to SELL to the client
+ * returns the orders that have the corresponding type
  */
-app.use('/orders/sell', (req, res) => {
+app.use('/orders/:type', (req, res) => {
     if (typeof (req.query.page) == "undefined") {
         page = 0
     } else {
         page = req.query.page;
-
     }
-    importOutage('SELL', page).then((data) => {
-
-        res.send(data);
-    })
-
-});
-
-/**
- * returns the orders that have a type set to Buy to the client
- */
-app.use('/orders/buy', (req, res) => {
-    if (typeof (req.query.page) == "undefined") {
-        page = 0
+    if ((req.baseUrl).toLocaleLowerCase() == '/orders/sell') {
+        importOrderBook('SELL', page).then((data) => {
+            res.send(data);
+        })
+    } else if ((req.baseUrl).toLocaleLowerCase() == '/orders/buy') {
+        importOrderBook('BUY', page).then((data) => {
+            res.send(data);
+        })
     } else {
-        page = req.query.page;
-
+        importOrderBook('ALL', page).then((data) => {
+            res.send(data);
+        })
     }
-    importOutage('BUY', page).then((data) => {
-        res.send(data);
-    })
-});
-
-/**
- * return all the data, regardless of the type to the client
- */
-app.use('/orders/all', (req, res) => {
-    if (typeof (req.query.page) == "undefined") {
-        page = 0
-    } else {
-        page = req.query.page;
-
-    }
-    importOutage('ALL', page).then((data) => {
-
-        res.send(data);
-    })
-});
+})
 
 app.use('/', (req, res) => {
-    importOutage().then((outageData) => {
-        res.send(outageData);
-    })
+    res.send("API WORKS")
 });
 
-
-
-app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
 
 const port = process.env.PORT || '3001';
 app.set('port', port);
@@ -80,18 +48,18 @@ server.listen(port, () => console.log(`API running on localhost:${port}`));
  * @param {*type of value that we want to have it takes 3 values => BUY, SELL or ALL} type 
  * @param {*The page number that the client want} page 
  */
-var importOutage = (type, page) => {
+var importOrderBook = (type, page) => {
     return new Promise(function (resolve, reject) {
         fs.readFile('../large-dataset.json', (err, data) => {
             if (err) throw err;
-            outageRawData = JSON.parse(data);
-            outageRawData = outageRawData.orders;
+            orderBookRawData = JSON.parse(data);
+            orderBookRawData = orderBookRawData.orders;
             if (type == "BUY") {
-                outageRawData = outageRawData.filter(value => value.type == 'BUY');
+                orderBookRawData = orderBookRawData.filter(value => value.type == 'BUY');
             } else if (type == "SELL") {
-                outageRawData = outageRawData.filter(value => value.type == 'SELL');
+                orderBookRawData = orderBookRawData.filter(value => value.type == 'SELL');
             }
-            outageRawData = outageRawData.sort((value1, value2) => {
+            orderBookRawData = orderBookRawData.sort((value1, value2) => {
                 tmp1 = new Date(value1.datetime);
                 tmp2 = new Date(value2.datetime)
                 if (tmp1 > tmp2) {
@@ -100,12 +68,14 @@ var importOutage = (type, page) => {
                     return -1
                 }
             })
-            numberPages = outageRawData.length / 10;
+            numberPages = orderBookRawData.length / 10;
             //console.log(numberPages)
             //console.log(Math.ceil(numberPages))
 
-            numberPages = Math.ceil(outageRawData.length / 10);
-            var outageResultData = [];
+            numberPages = Math.ceil(orderBookRawData.length / 10);
+            var orderBookResultData={}
+            orderBookResultData['totalPages'] =numberPages;
+            orderBookResultData['orders'] = [];
             if (page >= numberPages) {
                 resolve({ Error: 'Number of pages Exceeded' })
             } else {
@@ -114,14 +84,14 @@ var importOutage = (type, page) => {
                 var max = page * 10 + 10
                 for (i; i < max; i++) {
                     // console.log(i)
-                    if (outageRawData.length <= i) {
+                    if (orderBookRawData.length <= i) {
                         break;
                     } else {
-                        //console.log(outageRawData[i])
-                        outageResultData.push(outageRawData[i]);
+                        //console.log(orderBookRawData[i])
+                        orderBookResultData['orders'].push(orderBookRawData[i]);
                     }
                 }
-                resolve(outageResultData);
+                resolve(orderBookResultData);
             }
         });
     });
